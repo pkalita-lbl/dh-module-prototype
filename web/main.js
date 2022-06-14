3ad7d95faec082f0d0ce7e6e2849b60b93cf2ef1
Patrick Kalita <pkalita@lbl.gov>
2022-06-14 10:04:14 -0700

    Use subpath import of lib in web

--- web/main.js
+++ web/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import { DataDoodad } from '../lib'
+import { DataDoodad } from '#lib'
 
 const root = document.getElementById('app')
 const doodad = new DataDoodad(root, 'Somebody')
